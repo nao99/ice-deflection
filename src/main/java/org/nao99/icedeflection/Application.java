a3b66e4f28df82eff5e8dda932947938c808af28
Nikolai Osipov <nao99.dev@gmail.com>
2021-01-29 19:27:31 +0700

    feat(Q): implement q matrices calculation

--- src/main/java/org/nao99/icedeflection/Application.java
+++ src/main/java/org/nao99/icedeflection/Application.java
@@ -1,7 +1,6 @@
 package org.nao99.icedeflection;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.DiagonalMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
 import java.util.ArrayList;
@@ -11,13 +10,17 @@ import java.util.List;
 import static java.lang.Math.*;
 
 /**
- * Application class
+ * Application class <br>
+ *
+ * All variables called as common letters specially for
+ * more accurate match with the task itself
  *
  * @author  Nikolai Osipov <nao99.dev@gmail.com>
  * @version 1.0.0
  * @since   2021-01-23
  */
 public class Application {
+    private static final double G = 9.8;
     private static final int Y_BOUNDARY_LEFT  = -1;
     private static final int Y_BOUNDARY_RIGHT = 1;
 
@@ -27,34 +30,35 @@ public class Application {
      * @param args arguments
      */
     public static void main(String[] args) {
+        // 0. Entire physical parameters:
+        double hi = 0.1;
+        double L = 10.0;
+        double H = 2.0;
+        double E = 4.2 * pow(10, 9);
+        double tau = 0.1;
+        double nu = 0.3;
+        double D = (E * pow(hi, 3)) / (12 * (1 - pow(nu, 2)));
+        double pi = 917.0;
+        double p = 1000;
+        double U = 7.0;
+        double p0 = 1000;
+
+        double M = pi * hi;
+        double h = H / L;
+        double beta = D / (p * G * pow(L, 4));
+        double eps = (tau * U) / L;
+        double alpha = M / (p * L);
+        double Fr = U / sqrt(G * H);
+
         // 1. Entire the main parameters:
-        //  - voguesN = vogues count (voguesN > 0),
-        //  - x = step 1 (-infinity < x < +infinity)
-        //  - y = step 2 (-1 < y < 1)
-        //  - ξ = [..., ...] (for the first time will be written manually)
-        //  ...
+        int psiN = Integer.parseInt(args[0]);
+        double pisY = Double.parseDouble(args[1]);
 
-        int voguesN = Integer.parseInt(args[0]);
-        if (0 >= voguesN || 6 < voguesN) { // 6 boundary exists only because of we have 6 calculated lambdas
-            throw new IllegalArgumentException(String.format("Vogues count must be positive, but %d given", voguesN));
-        }
-
-        // -1 < y < 1
-        double y = Double.parseDouble(args[2]);
-        if (Y_BOUNDARY_LEFT >= y || y >= Y_BOUNDARY_RIGHT) {
-            throw new IllegalArgumentException(String.format("Incorrect y: %f (-1 < y < 1 excepted)", y));
-        }
-
-        y = 0.0125;
-        int stepsNumber = (int) ((Y_BOUNDARY_RIGHT - Y_BOUNDARY_LEFT) / y);
-
-        double[] ksiValues = {0.001};
+        int yStepsNumber = (int) ((Y_BOUNDARY_RIGHT - Y_BOUNDARY_LEFT) / pisY);
+        double[] ksiArray = {0.001};
 
         // 2. Calculate odd and even lambdas (λc[j], λs[j], where c - even, s - odd)
-        // Since we have already calculated them, we won't do it again. Just copy it
-        // We don't need recalculate them because they are always constants
-
-        double[] lambdasEven = {
+        double[] lambdaArrayEven = {
             2.365020372431352,
             5.497803919000836,
             8.63937982869974,
@@ -63,7 +67,7 @@ public class Application {
             18.06415775814131,
         };
 
-        double[] lambdasOdd = {
+        double[] lambdaArrayOdd = {
             3.927378719118806,
             7.068584195523235,
             10.21017612552063,
@@ -72,53 +76,48 @@ public class Application {
             19.63495408493621,
         };
 
-        // 3. Calculate A and B values for odd and even cases (it need to calculate C and M matrices in the future)
-        // Bc[j] = cos(λc[j]) / cosh(λc[j]), Ac[j] = sqrt(1 / (1 + Bc[j]^2))
-        // Bs[j] = sin(λs[j]) / sinh(λs[j]), As[j] = sqrt(1 / (1 - Bs[j]^2))
+        // 3. Calculate A and B values for odd and even cases (they are needed to calculate C and M matrices)
+        double[] BArrayEven = Arrays.stream(lambdaArrayEven).map(l -> cos(l) / cosh(l)).toArray();
+        double[] AArrayEven = Arrays.stream(BArrayEven).map(B -> sqrt(1 / (1 + B * B))).toArray();
 
-        double[] BValuesEven = Arrays.stream(lambdasEven).map(l -> cos(l) / cosh(l)).toArray();
-        double[] AValuesEven = Arrays.stream(BValuesEven).map(B -> sqrt(1 / (1 + B * B))).toArray();
+        double[] BArrayOdd = Arrays.stream(lambdaArrayOdd).map(l -> sin(l) / sinh(l)).toArray();
+        double[] AArrayOdd = Arrays.stream(BArrayOdd).map(B -> sqrt(1 / (1 - B * B))).toArray();
 
-        double[] BValuesOdd = Arrays.stream(lambdasOdd).map(l -> sin(l) / sinh(l)).toArray();
-        double[] AValuesOdd = Arrays.stream(BValuesOdd).map(B -> sqrt(1 / (1 - B * B))).toArray();
+        // 4. Calculate psi (vogues) (ψc[j], ψs[j], where c - even, s - odd)
+        RealMatrix psiMatrixEven = new Array2DRowRealMatrix(psiN, yStepsNumber);
+        RealMatrix psiMatrixOdd = new Array2DRowRealMatrix(psiN, yStepsNumber);
 
-        // 4. Calculate vogues (ψc[j], ψs[j], where c - even, s - odd)
+        for (int i = 0; i < psiN; i++) {
+            double lambdaEven = lambdaArrayEven[i];
+            double lambdaOdd = lambdaArrayOdd[i];
 
-        RealMatrix voguesEven = new Array2DRowRealMatrix(voguesN, stepsNumber);
-        RealMatrix voguesOdd = new Array2DRowRealMatrix(voguesN, stepsNumber);
+            double AEven = AArrayEven[i];
+            double AOdd = AArrayOdd[i];
 
-        for (int j = 0; j < voguesN; j++) {
-            double lambdaEven = lambdasEven[j];
-            double lambdaOdd = lambdasOdd[j];
+            double BEven = BArrayEven[i];
+            double BOdd = BArrayOdd[i];
 
-            double AEven = AValuesEven[j];
-            double AOdd = AValuesOdd[j];
+            for (int j = 0; j < yStepsNumber; j++) {
+                double stepValue = Y_BOUNDARY_LEFT + pisY * j;
 
-            double BEven = BValuesEven[j];
-            double BOdd = BValuesOdd[j];
+                double psiEven = AEven * (cos(lambdaEven * stepValue) - BEven * cosh(lambdaEven * stepValue));
+                psiMatrixEven.addToEntry(i, j, psiEven);
 
-            for (int stepNumber = 0; stepNumber < stepsNumber; stepNumber++) {
-                double stepValue = Y_BOUNDARY_LEFT + y * stepNumber;
-
-                double vogueEven = AEven * (cos(lambdaEven * stepValue) - BEven * cosh(lambdaEven * stepValue));
-                voguesEven.addToEntry(j, stepNumber, vogueEven);
-
-                double vogueOdd = AOdd * (sin(lambdaOdd * stepValue) - BOdd * sinh(lambdaOdd * stepValue));
-                voguesOdd.addToEntry(j, stepNumber, vogueOdd);
+                double psiOdd = AOdd * (sin(lambdaOdd * stepValue) - BOdd * sinh(lambdaOdd * stepValue));
+                psiMatrixOdd.addToEntry(i, j, psiOdd);
             }
         }
 
         // 5. Calculate C matrices for even and odd cases (Cc[j], Cs[j], where c - even, s - odd)
+        RealMatrix CMatrixEven = new Array2DRowRealMatrix(psiN, psiN);
+        RealMatrix CMatrixOdd = new Array2DRowRealMatrix(psiN, psiN);
 
-        RealMatrix CValuesEven = new Array2DRowRealMatrix(voguesN, voguesN);
-        RealMatrix CValuesOdd = new Array2DRowRealMatrix(voguesN, voguesN);
+        for (int n = 0; n < psiN; n++) {
+            double lambdaEvenN = lambdaArrayEven[n];
+            double lambdaOddN = lambdaArrayOdd[n];
 
-        for (int n = 0; n < voguesN; n++) {
-            double lambdaEvenN = lambdasEven[n];
-            double lambdaOddN = lambdasOdd[n];
-
-            double AEvenN = AValuesEven[n];
-            double AOddN = AValuesOdd[n];
+            double AEvenN = AArrayEven[n];
+            double AOddN = AArrayOdd[n];
 
             double lambdaEvenNSquare = lambdaEvenN * lambdaEvenN;
             double lambdaOddNSquare = lambdaOddN * lambdaOddN;
@@ -132,15 +131,15 @@ public class Application {
             double lambdaEvenNSin = sin(lambdaEvenN);
             double lambdaOddNSin = sin(lambdaOddN);
 
-            for (int m = 0; m < voguesN; m++) {
-                double lambdaEvenM = lambdasEven[m];
-                double lambdaOddM = lambdasOdd[m];
+            for (int m = 0; m < psiN; m++) {
+                double lambdaEvenM = lambdaArrayEven[m];
+                double lambdaOddM = lambdaArrayOdd[m];
 
                 double lambdaEvenMSquare = lambdaEvenM * lambdaEvenM;
                 double lambdaOddMSquare = lambdaOddM * lambdaOddM;
 
-                double AEvenM = AValuesEven[m];
-                double AOddM = AValuesOdd[m];
+                double AEvenM = AArrayEven[m];
+                double AOddM = AArrayOdd[m];
 
                 double lambdaEvenMCos = cos(lambdaEvenM);
                 double lambdaOddMCos = cos(lambdaOddM);
@@ -175,69 +174,95 @@ public class Application {
                     COdd *= lambdaOddN * lambdaOddNCos * lambdaOddMSin - lambdaOddM * lambdaEvenMCos * lambdaOddNSin;
                 }
 
-                CValuesEven.addToEntry(n, m, CEven);
-                CValuesOdd.addToEntry(n, m, COdd);
+                CMatrixEven.addToEntry(n, m, CEven);
+                CMatrixOdd.addToEntry(n, m, COdd);
             }
         }
 
         // 6. Calculate M matrices for even and odd cases (Mc[j], Ms[j], where c - even, s - odd)
+        List<RealMatrix> MMatricesEven = new ArrayList<>();
+        List<RealMatrix> MMatricesOdd = new ArrayList<>();
 
-        // μc[j] = sqrt(ξ * ξ + (PI * j)^2)
-        // μs[j] = sqrt(ξ * ξ + (PI * j - PI / 2)^2)
+        for (double ksi : ksiArray) {
+            RealMatrix MMatrixEven = new Array2DRowRealMatrix(psiN, psiN);
+            RealMatrix MMatrixOdd = new Array2DRowRealMatrix(psiN, psiN);
 
-        List<RealMatrix> MsValuesEven = new ArrayList<>();
-        List<RealMatrix> MsValuesOdd = new ArrayList<>();
+            double ksiSquared = ksi * ksi;
 
-        double h = 0.1;
-        for (double ksi : ksiValues) {
-            RealMatrix MValuesEven = new Array2DRowRealMatrix(voguesN, voguesN);
-            RealMatrix MValuesOdd = new Array2DRowRealMatrix(voguesN, voguesN);
+            for (int m = 0; m < psiN; m++) {
+                for (int n = 0; n < psiN; n++) {
+                    // TODO: Specify about psiMatrix. May be it's not a truly psi (vogues) matrix
 
-            for (int m = 0; m < voguesN; m++) {
-                for (int n = 0; n < voguesN; n++) {
-                    double MEven = voguesEven.getEntry(0, m) * voguesEven.getEntry(0, n)
+                    double MEven = psiMatrixEven.getEntry(0, m) * psiMatrixEven.getEntry(0, n)
                         / (2 * ksi * tanh(ksi * h));
 
                     double MOdd = 0.0;
 
-                    for (int j = 1; j < voguesN; j++) {
-                        double muEven = sqrt(ksi * ksi + pow(PI * j, 2));
-                        double muOdd = sqrt(ksi * ksi + pow(PI * j - PI / 2, 2));
+                    for (int j = 1; j < psiN; j++) {
+                        double muEven = sqrt(ksiSquared + pow(PI * j, 2));
+                        double muOdd = sqrt(ksiSquared + pow(PI * j - PI / 2, 2));
 
-                        MEven += voguesEven.getEntry(j, m) * voguesEven.getEntry(j, n) / muEven * tanh(muEven * h);
-                        MOdd += voguesOdd.getEntry(j, m) * voguesOdd.getEntry(j, n) / muOdd * tanh(muOdd * h);
+                        MEven += psiMatrixEven.getEntry(j, m) * psiMatrixEven.getEntry(j, n) / muEven * tanh(muEven * h);
+                        MOdd += psiMatrixOdd.getEntry(j, m) * psiMatrixOdd.getEntry(j, n) / muOdd * tanh(muOdd * h);
                     }
 
-                    MValuesEven.addToEntry(m, n, MEven);
-                    MValuesOdd.addToEntry(m, n, MOdd);
+                    MEven *= ksiSquared;
+                    MOdd *= ksiSquared;
+
+                    MMatrixEven.addToEntry(m, n, MEven);
+                    MMatrixOdd.addToEntry(m, n, MOdd);
                 }
             }
 
-            MsValuesEven.add(MValuesEven);
-            MsValuesOdd.add(MValuesOdd);
+            MMatricesEven.add(MMatrixEven);
+            MMatricesOdd.add(MMatrixOdd);
         }
 
         // 7. Calculate D matrices for even and odd cases (Dc[j], Ds[j], where c - even, s - odd)
+        List<RealMatrix> DMatricesEven = new ArrayList<>();
+        List<RealMatrix> DMatricesOdd = new ArrayList<>();
 
-        List<RealMatrix> DsValuesEven = new ArrayList<>();
-        List<RealMatrix> DsValuesOdd = new ArrayList<>();
+        for (double ksi : ksiArray) {
+            RealMatrix DMatrixEven = new Array2DRowRealMatrix(psiN, psiN);
+            RealMatrix DMatrixOdd = new Array2DRowRealMatrix(psiN, psiN);
 
-        for (double ksi : ksiValues) {
-            RealMatrix DValuesEven = new DiagonalMatrix(voguesN);
-            RealMatrix DValuesOdd = new DiagonalMatrix(voguesN);
+            double ksiFourth = pow(ksi, 4);
 
-            double ksiPowered = pow(ksi, 4);
+            for (int i = 0; i < psiN; i++) {
+                double DEven = pow(lambdaArrayEven[i], 4) + ksiFourth;
+                double DOdd = pow(lambdaArrayOdd[i], 4) + ksiFourth;
 
-            for (int i = 0; i < voguesN; i++) {
-                double DEven = pow(lambdasEven[i], 4) + ksiPowered;
-                double DOdd = pow(lambdasOdd[i], 4) + ksiPowered;
-
-                DValuesEven.addToEntry(i, i, DEven);
-                DValuesOdd.addToEntry(i, i, DOdd);
+                DMatrixEven.addToEntry(i, i, DEven);
+                DMatrixOdd.addToEntry(i, i, DOdd);
             }
 
-            DsValuesEven.add(DValuesEven);
-            DsValuesOdd.add(DValuesOdd);
+            DMatricesEven.add(DMatrixEven);
+            DMatricesOdd.add(DMatrixOdd);
         }
+
+        // 8. Calculate Q matrices for even and odd cases (Qc[j], Qs[j], where c - even, s - odd)
+        List<RealMatrix> QMatricesEven = new ArrayList<>();
+        List<RealMatrix> QMatricesOdd = new ArrayList<>();
+
+        int i = 0; // will be removed after code refractored and optimized
+        for (double ksi : ksiArray) {
+            double ksiSquared = ksi * ksi;
+
+            RealMatrix DMatrixEven = DMatricesEven.get(i);
+            RealMatrix DMatrixOdd = DMatricesOdd.get(i);
+
+            RealMatrix KsiCMatrixEven = CMatrixEven.scalarMultiply(2 * ksiSquared);
+            RealMatrix KsiCMatrixOdd = CMatrixOdd.scalarMultiply(2 * ksiSquared);
+
+            RealMatrix QMatrixEven = DMatrixEven.subtract(KsiCMatrixEven);
+            RealMatrix QMatrixOdd = DMatrixOdd.subtract(KsiCMatrixOdd);
+
+            QMatricesEven.add(QMatrixEven);
+            QMatricesOdd.add(QMatrixOdd);
+
+            i++;
+        }
+
+        String a = "";
     }
 }
